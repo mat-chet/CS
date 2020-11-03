5e0a9dd8b87aabe148160b460517da1d4f067813
unknown <C:\Users\Matvey\AppData\Roaming\The Bat!>
2020-11-03 12:18:41 +0300

    IdP

---
+++ Saml/AuthApp/Saml/ResponseTool.cs
@@ -0,0 +1,113 @@
+﻿using Saml;
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace AuthApp.Controllers
+{
+    public class ResponseTool
+    {
+        public static XmlDocument CreateResponse(SamlRequestReader req)
+        {
+            //SamlRequestReader req = new SamlRequestReader(request);
+            XmlDocument response = new SamlResponseXMLBuilder()
+                .SetInResponseTo(req.GetAuthnRequestID())
+                .SetDestination(req.GetAssertionConsumerServiceURL())
+                .SetIssuer(req.GetIssuer())
+                .SetNameID(req.GetNameID())
+                .SetAudience(req.GetIssuer())
+                .SetNotOnAfter(DateTime.Now.AddDays(365).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"))
+                .SetIdResponse("_" + System.Guid.NewGuid().ToString())
+                .SetIdAssertion("_" + System.Guid.NewGuid().ToString())
+                .SetNotBefore(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"))
+                .SetSessionIndex("_samlong_")
+                .SetIssueInstantAssertion(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"))
+                .SetIssueInstantResponse(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"))
+                .SetSessionNotOnOrAfter(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"))
+                .Build();
+
+
+            return response;
+        }
+
+        public static XmlDocument SignXml(string XMLtext, string SubjectName, string privateKey)
+        {
+            if (null == XMLtext)
+                throw new ArgumentNullException("XMLtext");
+            if (null == SubjectName)
+                throw new ArgumentNullException("SubjectName");
+
+            // Load the certificate from the certificate store.
+            X509Certificate2 cert = new X509Certificate2(StringToByteArray(SubjectName));
+
+            // Create a new XML document.
+            XmlDocument doc = new XmlDocument();
+
+            // Format the document to ignore white spaces.
+            doc.PreserveWhitespace = false;
+
+            // Load the passed XML file using it's name.
+            doc.LoadXml(XMLtext);
+
+            // Create a SignedXml object.
+            SignedXml signedXml = new SignedXml(doc);
+
+            RSA rsa = new RSACng();
+            rsa.ImportRSAPrivateKey(Convert.FromBase64String(privateKey), out _);
+            // Add the key to the SignedXml document. 
+            signedXml.SigningKey = rsa;
+
+            // Create a reference to be signed.
+            Reference reference = new Reference();
+            reference.Uri = "";
+
+            // Add an enveloped transformation to the reference.
+            XmlDsigEnvelopedSignatureTransform env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            // Add the reference to the SignedXml object.
+            signedXml.AddReference(reference);
+
+            // Create a new KeyInfo object.
+            KeyInfo keyInfo = new KeyInfo();
+
+            // Load the certificate into a KeyInfoX509Data object
+            // and add it to the KeyInfo object.
+            keyInfo.AddClause(new KeyInfoX509Data(cert));
+
+            // Add the KeyInfo object to the SignedXml object.
+            signedXml.KeyInfo = keyInfo;
+
+            // Compute the signature.
+            signedXml.ComputeSignature();
+
+            // Get the XML representation of the signature and save
+            // it to an XmlElement object.
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+
+            // Append the element to the XML document.
+            doc.DocumentElement.AppendChild(doc.ImportNode(xmlDigitalSignature, true));
+
+            if (doc.FirstChild is XmlDeclaration)
+            {
+                doc.RemoveChild(doc.FirstChild);
+            }
+
+            // Save the signed XML document to a file specified
+            // using the passed string.
+            return doc;
+        }
+
+        public static byte[] StringToByteArray(string st)
+        {
+            byte[] bytes = new byte[st.Length];
+            for (int i = 0; i < st.Length; i++)
+            {
+                bytes[i] = (byte)st[i];
+            }
+            return bytes;
+        }
+    }
+}
---
+++ Saml/AuthApp/Saml/SamlResponseXMLBuilder.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace Saml
+{
+    class SamlResponseXMLBuilder
+    {
+		
+
+        public string idAssertion = "1";
+
+		public SamlResponseXMLBuilder SetIdAssertion(string id)
+        {
+			this.idAssertion = id;
+			return this;
+
+		}
+
+        public string issue_instant_Assertion = "1";
+
+		public SamlResponseXMLBuilder SetIssueInstantAssertion(string IssueInstantAssertion)
+		{
+			this.issue_instant_Assertion = IssueInstantAssertion;
+			return this;
+
+		}
+
+		public string notOnAfter = "1";
+		public SamlResponseXMLBuilder SetNotOnAfter(string notOnAfter)
+		{
+			this.notOnAfter = notOnAfter;
+			return this;
+
+		}
+
+		public string idResponse = "1";
+		public SamlResponseXMLBuilder SetIdResponse(string id)
+		{
+			this.idResponse = id;
+			return this;
+
+		}
+
+		public string issue_instant_Response = "1";
+		public SamlResponseXMLBuilder SetIssueInstantResponse(string issue_instant_Response)
+		{
+			this.issue_instant_Response = issue_instant_Response;
+			return this;
+
+		}
+
+		public string inResponseTo = "1";
+		public SamlResponseXMLBuilder SetInResponseTo(string inResponseTo)
+		{
+			this.inResponseTo = inResponseTo;
+			return this;
+
+		}
+
+		public string destination = "1";
+		public SamlResponseXMLBuilder SetDestination(string destination)
+		{
+			this.destination = destination;
+			return this;
+
+		}
+
+		public string issuer = "1";
+		public SamlResponseXMLBuilder SetIssuer(string issuer)
+		{
+			this.issuer = issuer;
+			return this;
+
+		}
+
+		public string nameID = "1";
+		public SamlResponseXMLBuilder SetNameID(string nameID)
+		{
+			this.nameID = nameID;
+			return this;
+
+		}
+
+		public string notBefore = "1";
+		public SamlResponseXMLBuilder SetNotBefore(string notBefore)
+		{
+			this.notBefore = notBefore;
+			return this;
+
+		}
+
+		public string audience = "1";
+		public SamlResponseXMLBuilder SetAudience(string audience)
+		{
+			this.audience = audience;
+			return this;
+
+		}
+
+		public string authnInstant = "1";
+		public SamlResponseXMLBuilder SetAuthnInstant(string authnInstant)
+		{
+			this.authnInstant = authnInstant;
+			return this;
+
+		}
+
+		public string sessionNotOnOrAfter = "1";
+		public SamlResponseXMLBuilder SetSessionNotOnOrAfter(string sessionNotOnOrAfter)
+		{
+			this.sessionNotOnOrAfter = sessionNotOnOrAfter;
+			return this;
+
+		}
+
+		public string sessionIndex = "1";
+		public SamlResponseXMLBuilder SetSessionIndex(string sessionIndex)
+		{
+			this.sessionIndex = sessionIndex;
+			return this;
+
+		}
+
+
+
+
+		public XmlDocument Build()
+        {
+			using (StringWriter sw = new StringWriter())
+			{
+				XmlWriterSettings xws = new XmlWriterSettings();
+				xws.OmitXmlDeclaration = true;
+
+				using (XmlWriter xw = XmlWriter.Create(sw, xws))
+				{
+					//
+					xw.WriteStartElement("samlp", "Response", "urn:oasis:names:tc:SAML:2.0:protocol");
+					xw.WriteAttributeString("xmlns", "saml",null, "urn:oasis:names:tc:SAML:2.0:assertion");
+					xw.WriteAttributeString("ID", idResponse);
+					xw.WriteAttributeString("Version", "2.0");
+					xw.WriteAttributeString("IssueInstant", issue_instant_Response);
+					xw.WriteAttributeString("InResponseTo", inResponseTo);
+					xw.WriteAttributeString("Destination", destination);
+
+					//
+					xw.WriteStartElement("saml", "Issuer",null);
+					xw.WriteString(issuer);
+					xw.WriteEndElement();
+					//
+
+					//
+					xw.WriteStartElement("saml", "Status", null);
+					xw.WriteStartElement("saml", "StatusCode", null);
+					xw.WriteAttributeString("Value", "urn:oasis:names:tc:SAML:2.0:status:Success");
+					xw.WriteEndElement();
+					xw.WriteEndElement();
+					//
+
+					//
+					xw.WriteStartElement("saml", "Assertion", null);
+					xw.WriteAttributeString("xmlns", "saml", null, "urn:oasis:names:tc:SAML:2.0:assertion");
+					xw.WriteAttributeString("Version", "2.0");
+					xw.WriteAttributeString("ID", idAssertion);
+					xw.WriteAttributeString("IssueInstant", issue_instant_Assertion);
+					
+
+					
+					//
+					xw.WriteStartElement("saml", "Issuer", null);
+					xw.WriteString(issuer);
+					xw.WriteEndElement();
+					//
+
+
+					//
+					xw.WriteStartElement("saml", "Subject", null);
+					xw.WriteStartElement("saml", "NameID", null);
+					xw.WriteAttributeString("Format", "urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified");
+					xw.WriteString(nameID);
+					xw.WriteEndElement();
+
+					xw.WriteStartElement("saml", "SubjectConfirmation", null);
+					xw.WriteAttributeString("Method", "urn:oasis:names:tc:SAML:2.0:cm:bearer");
+
+					xw.WriteStartElement("saml", "SubjectConfirmationData", null);
+					xw.WriteAttributeString("NotOnOrAfter", notOnAfter);
+					xw.WriteAttributeString("Recipient", destination);
+					xw.WriteAttributeString("InResponseTo", inResponseTo);
+					xw.WriteEndElement();
+
+					xw.WriteEndElement();
+
+					xw.WriteEndElement();//end subject
+					//
+
+
+					//
+					xw.WriteStartElement("saml", "Conditions", null);
+					xw.WriteAttributeString("NotBefore", issue_instant_Assertion);
+					xw.WriteAttributeString("NotOnOrAfter", notOnAfter);
+
+
+					xw.WriteStartElement("saml", "AudienceRestriction", null);
+					xw.WriteStartElement("saml", "Audience", null);
+					xw.WriteString(audience);
+					xw.WriteEndElement();
+					xw.WriteEndElement();
+
+					xw.WriteEndElement();//end conditions
+					//
+					
+
+					//
+					xw.WriteStartElement("saml", "AuthnStatement", null);
+					xw.WriteAttributeString("AuthnInstant", issue_instant_Assertion);
+					xw.WriteAttributeString("SessionNotOnOrAfter", sessionNotOnOrAfter);
+					xw.WriteAttributeString("SessionIndex", sessionIndex);
+
+
+					xw.WriteStartElement("saml", "AuthnContext", null);
+					xw.WriteStartElement("saml", "AuthnContextClassRef", null);
+					xw.WriteString("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+					xw.WriteEndElement();
+					xw.WriteEndElement();
+
+					xw.WriteEndElement();//end AuthnStatement
+					//
+
+
+					xw.WriteEndElement();
+					//
+
+
+					xw.WriteEndElement();
+
+				}
+
+				XmlDocument document = new XmlDocument();
+				document.LoadXml(sw.ToString());
+				return document;
+				
+
+			}
+		}
+    }
+}
